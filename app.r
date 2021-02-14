be7a1e65a238d861e295ffc5f7a78dabfa396e15
JRapt0r <jonrepta@gmail.com>
2021-02-13 20:13:48 -0600

    Everything complete, just need to iron out a few bugs

--- app.r
+++ app.r
@@ -322,7 +322,18 @@ ui <- fluidPage(
                     )
                   )
                 ),
-                tabPanel("Energy Usage Bar (percentage)", plotOutput("line1"), plotOutput("line5") ),
+                tabPanel("Energy Usage Bar (percentage)",
+                  fluidRow(
+                    column(6,
+                      plotOutput("line1"),
+                      plotOutput("line5")
+                    ),
+                    column(6,
+                      plotOutput("map1"),
+                      plotOutput("map5")
+                    )
+                  )
+                ),
                 tabPanel("Energy Usage Line",
                   fluidRow(
                     column(6,
@@ -330,11 +341,23 @@ ui <- fluidPage(
                       plotOutput("line6")
                     ),
                     column(6,
-                      plotOutput("map2")
+                      plotOutput("map2"),
+                      plotOutput("map6")
                     )
                   )
                 ),
-                tabPanel("Energy Usage Line (percentage)", plotOutput("line3"), plotOutput("line7") )
+                tabPanel("Energy Usage Line (percentage)",
+                  fluidRow(
+                    column(6,
+                      plotOutput("line3"),
+                      plotOutput("line7")
+                    ),
+                    column(6,
+                      plotOutput("map3"),
+                      plotOutput("map7")
+                    )
+                  )
+                )
             )
           )
       )
@@ -344,8 +367,8 @@ ui <- fluidPage(
           mainPanel(
             tabsetPanel(
               id = 'rawdata',
-              tabPanel("rawTotalsPerYear", DT::dataTableOutput("mytable0")),
-              tabPanel("rawTotalsPerYearPercent", DT::dataTableOutput("mytable1"))
+              tabPanel("Raw Energy Produced", DT::dataTableOutput("mytable0")),
+              tabPanel("Raw Energy Produced (percentage)", DT::dataTableOutput("mytable1"))
             )
           )
         )
@@ -698,6 +721,110 @@ server <- function(input, output) {
     toReturn
   })
 
+  mapProductionReactivePercentage1 <- reactive({
+    dataSet <- NULL
+    toReturn <- NULL
+
+    # All years
+    if (input$yearSelect1 == 0) {
+      dataSet <- energyWithoutTotal
+    }
+    else {
+      dataSet <- subset(energyWithoutTotal, energyWithoutTotal$year == input$yearSelect1)
+    }
+
+    if (input$check2) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Coal"))
+    }
+    if (input$check3) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Geothermal"))
+    }
+    if (input$check4) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Hydroelectric Conventional"))
+    }
+    if (input$check5) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Natural Gas"))
+    }
+    if (input$check6) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Nuclear"))
+    }
+    if (input$check7) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Petroleum"))
+    }
+    if (input$check8) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Solar Thermal and Photovoltaic"))
+    }
+    if (input$check9) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Wind"))
+    }
+    if (input$check10) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Wood and Wood Derived Fuels"))
+    }
+
+    # All
+    if (input$check1) {
+      toReturn <- dataSet
+    }
+
+    (toReturn <- ddply(toReturn, .(state), summarize, state_usage=sum(megaWattHours)))
+    total_usage = sum(toReturn$state_usage)
+    toReturn <- ddply(toReturn, .(state), mutate, state_percentage = (state_usage / total_usage)*100)
+
+    toReturn
+  })
+
+  mapProductionReactivePercentage2 <- reactive({
+    dataSet <- NULL
+    toReturn <- NULL
+
+    # All years
+    if (input$yearSelect2 == 0) {
+      dataSet <- energyWithoutTotal
+    }
+    else {
+      dataSet <- subset(energyWithoutTotal, energyWithoutTotal$year == input$yearSelect2)
+    }
+
+    if (input$check2) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Coal"))
+    }
+    if (input$check3) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Geothermal"))
+    }
+    if (input$check4) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Hydroelectric Conventional"))
+    }
+    if (input$check5) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Natural Gas"))
+    }
+    if (input$check6) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Nuclear"))
+    }
+    if (input$check7) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Petroleum"))
+    }
+    if (input$check8) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Solar Thermal and Photovoltaic"))
+    }
+    if (input$check9) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Wind"))
+    }
+    if (input$check10) {
+       toReturn <- rbind(toReturn, subset(dataSet, dataSet$energySource == "Wood and Wood Derived Fuels"))
+    }
+
+    # All
+    if (input$check1) {
+      toReturn <- dataSet
+    }
+
+    (toReturn <- ddply(toReturn, .(state), summarize, state_usage=sum(megaWattHours)))
+    total_usage = sum(toReturn$state_usage)
+    toReturn <- ddply(toReturn, .(state), mutate, state_percentage = (state_usage / total_usage)*100)
+
+    toReturn
+  })
+
 
   # Graph reactive graphs
   output$line0 <- renderPlot({
@@ -734,12 +861,22 @@ server <- function(input, output) {
     scale_y_continuous(labels=scales::percent)+
     scale_fill_manual(name = "Energy Sources", values = myColors)
   })
+  output$map1 <- renderPlot({
+    plot_usmap(regions="states", data=mapProductionReactivePercentage1(), values="state_percentage", labels=TRUE, label_color = "#ffffff")+
+    scale_fill_continuous(low = "#1e90ff", high = "#092b4c", name="Percent contributed", labels = function(x) format(x, big.mark=",", scientific = FALSE))+
+    labs(title="Energy Contribution (percentage)", subtitle="by Region")
+  })
   output$line5 <- renderPlot({
     ggplot(data=justOneEnergySourceReactive2(), aes(x = year, y = megaWattHours, fill = energySource))+
     geom_bar(stat="identity", position="fill")+
     labs(title=paste(input$stateSelect2,"Energy Contribution", sep=" "), subtitle="as Percentage of Total", x = "Year", y = "Percent contributed")+
     scale_y_continuous(labels=scales::percent)+
     scale_fill_manual(name = "Energy Sources", values = myColors)
+  })
+  output$map5 <- renderPlot({
+    plot_usmap(regions="states", data=mapProductionReactivePercentage2(), values="state_percentage", labels=TRUE, label_color = "#ffffff")+
+    scale_fill_continuous(low = "#1e90ff", high = "#092b4c", name="Percent contributed", labels = function(x) format(x, big.mark=",", scientific = FALSE))+
+    labs(title="Energy Contribution (percentage)", subtitle="by Region")
   })
 
 
@@ -762,31 +899,52 @@ server <- function(input, output) {
     scale_y_continuous(labels = function(x) format(x/1000000000, big.mark=",", scientific = FALSE))+
     scale_colour_manual(name = "Energy Sources", values = myColors)
   })
-
+  output$map6 <- renderPlot({
+    plot_usmap(regions="states", data=mapProductionReactive2(), values="state_usage", labels=TRUE,label_color = "#ffffff")+
+    scale_fill_continuous(low = "#1e90ff", high = "#092b4c", name="Energy Generated\n(in billion Mwh)", labels = function(x) format(x/1000000000, big.mark=",", scientific = FALSE))+
+    labs(title="Energy Contribution", subtitle="by Region")
+  })
 
   output$line3 <- renderPlot({
     ggplot(data=justOneEnergySourcePercentageReactive(), aes(x = year, y = yearly_percentage, color=energySource))+
     stat_summary(fun="sum", geom="line", size=1.0, show.legend=TRUE)+
     geom_point()+
-    labs(title=paste(input$stateSelect1,"Energy Contribution", sep=" "), subtitle="Over Time", x = "Year", y = "Percent contributed")+
+    labs(title=paste(input$stateSelect1,"Energy Contribution", sep=" "), subtitle="Over Time (as percentage of total)", x = "Year", y = "Percent contributed")+
     scale_y_continuous(labels=scales::percent)+
     scale_colour_manual(name = "Energy Sources", values = myColors)
   })
+  output$map3 <- renderPlot({
+    plot_usmap(regions="states", data=mapProductionReactivePercentage1(), values="state_percentage", labels=TRUE, label_color = "#ffffff")+
+    scale_fill_continuous(low = "#1e90ff", high = "#092b4c", name="Percent contributed", labels = function(x) format(x, big.mark=",", scientific = FALSE))+
+    labs(title="Energy Contribution (percentage)", subtitle="by Region")
+  })
+
   output$line7 <- renderPlot({
     ggplot(data=justOneEnergySourcePercentageReactive2(), aes(x = year, y = yearly_percentage, color=energySource))+
     stat_summary(fun="sum", geom="line", size=1.0, show.legend=TRUE)+
     geom_point()+
-    labs(title=paste(input$stateSelect2,"Energy Contribution", sep=" "), subtitle="Over Time", x = "Year", y = "Percent contributed")+
+    labs(title=paste(input$stateSelect2,"Energy Contribution", sep=" "), subtitle="Over Time (as percentage of total)", x = "Year", y = "Percent contributed")+
     scale_y_continuous(labels=scales::percent)+
     scale_colour_manual(name = "Energy Sources", values = myColors)
   })
+  output$map7 <- renderPlot({
+    plot_usmap(regions="states", data=mapProductionReactivePercentage2(), values="state_percentage", labels=TRUE, label_color = "#ffffff")+
+    scale_fill_continuous(low = "#1e90ff", high = "#092b4c", name="Percent contributed", labels = function(x) format(x, big.mark=",", scientific = FALSE))+
+    labs(title="Energy Contribution (percentage)", subtitle="by Region")
+  })
+
 
   output$mytable0 <- DT::renderDataTable({
-    DT::datatable(rawTotalsPerYear, options = list(orderClasses = TRUE))
+    rt <- rawTotalsPerYear
+    names(rt) <- c("Year","Energy Source","Energy Produced (Mwh)")
+    DT::datatable(rt, options = list(orderClasses = TRUE))
   })
 
   output$mytable1 <- DT::renderDataTable({
-    DT::datatable(percentContributionPerYear, options = list(orderClasses = TRUE))
+    (pc <- ddply(energyWithoutTotal, .(year, energySource), summarize, yearly_usage=sum(megaWattHours)))
+    pc <- ddply(pc, .(year), mutate, yearly_percentage = (yearly_usage / sum(yearly_usage)) * 100)
+    names(pc) <- c("Year", "Energy Source", "Energy Produced (mwH)", "Percent of Yearly Total")
+    DT::datatable(pc, options = list(orderClasses = TRUE))
   })
 
   # About page
